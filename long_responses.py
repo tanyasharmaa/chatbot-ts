2bb59447edbea62efa729f240e07b5f5c47874e8
Tanya Sharma <sharmatanya25.1100@gmail.com>
2022-03-20 16:48:56 +0530

    new commit

---
+++ long_responses.py
@@ -0,0 +1,11 @@
+import random
+
+R_EATING = " i dont like eating anything because i am a bot obviously!"
+
+
+
+def unknown():
+    response =['could you please re-phrase that? ',
+                "...",
+                "sounds about right",
+                "what does that mean? "][random.randrange(4)]
